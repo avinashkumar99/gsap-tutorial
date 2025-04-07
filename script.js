bca70427a0f1f61dce20696adb1e80096a1b9692
avinash <avinashhkumar99@gmail.com>
2025-04-07 23:52:37 +0530

    to and from tweens

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 gsap.to(".box1", { x: 500, duration: 5, ease: "bounce.out" });
 
 gsap.from(".box2", { x: 500, duration: 5, ease: "bounce.out" });
+
+gsap.to(".box3", { rotation: 360, x: 200, yPercent: -150, duration: 3 });
