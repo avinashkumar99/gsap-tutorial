3bec9be32c37796a3bb704936b58c2f886c14c84
avinash <avinashhkumar99@gmail.com>
2025-04-08 23:50:42 +0530

    added

--- script.js
+++ script.js
@@ -2,4 +2,15 @@ gsap.to(".box1", { x: 500, duration: 5, ease: "bounce.out" });
 
 gsap.from(".box2", { x: 500, duration: 5, ease: "bounce.out" });
 
-gsap.to(".box3", { rotation: 360, x: 200, yPercent: -150, duration: 3 });
+gsap.to(".box3", {
+  scale: 1.1,
+  rotation: 360,
+  x: 30,
+  yPercent: 10,
+  duration: 3,
+  ease: "back",
+  skew: 30,
+  //   opacity: 0,
+  repeat: -1,
+  // yoyo: true,
+});
